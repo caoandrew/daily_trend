6cfc38e8e5b15e87468fc5a115dfe7f1a9e7660d
MilesYang03 <86486135+MilesYang03@users.noreply.github.com>
2022-10-25 20:28:45 -0400

    Created a header
    Not sure if the header sticks when scrolling yet

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ScreenStack } from "react-native-screens";
 
-export default function App() {
+// https://www.geeksforgeeks.org/react-native-configuring-header-bar/
+
+function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
     </View>
+  )
+}
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
