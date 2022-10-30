b88f32f3e57dd0d603665d6c95b5344ac69fcf90
MilesYang03 <86486135+MilesYang03@users.noreply.github.com>
2022-10-30 17:02:16 -0400

    Update Header.js

--- Components/Header.js
+++ Components/Header.js
@@ -6,7 +6,6 @@ class Header extends Component {
   render() {
     return (
       <View style={styles.container}>
-        {/* <Text> Home </Text> */}
         <StatusBar style="auto" />
       </View>
     )
